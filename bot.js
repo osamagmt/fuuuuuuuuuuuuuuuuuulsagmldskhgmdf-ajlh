d0e1cd1b356142f3a7c28811e5594fd34264d6ff
nachat70 <38468727+nachat70@users.noreply.github.com>
2018-07-25 12:42:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,7 +48,85 @@ client.on('message', message => {
 
 
 
+client.on('message',async message => {
+let mention = message.mentions.members.first();
+let role = message.content.split(" ").slice(2).join(" ");
+let mySupport = message.guild.roles.find('name',role);
+let acRoom = client.channels.get('471067470212497409');
+if(message.content.startsWith(prefix + "قبول")) {
+  if(message.guild.id !== '455577214348427264') return;
+  if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return;
+  if(!mention) return message.reply('منشن شخص');
+  if(!role) return message.reply('ادخل اسم رتبة');
+  if(!mySupport) return message.reply('هذه الرتبة غير موجودة');
+  if(mention.roles.has(mySupport)) return message.reply('هذا الشخص معه الرتبة مسبقا');
 
+  mention.addRole(mySupport).then(() => {
+    acRoom.send(`**[ ${mySupport} ] واعطائك رتبة ${mention} تم بنجاح قبولك**`);
+  });
+}
+});
+
+
+
+client.on('message',async message => {
+let mention = message.mentions.members.first();
+let acRoom = client.channels.get('471067470212497409');
+if(message.content.startsWith(prefix + "رفض")) {
+if(message.guild.id !== '455577214348427264') return;
+if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return;
+if(!mention) return message.reply("منشن شخص");
+
+acRoom.send(`**${mention} تم رفضك للاسف**`)
+}
+
+})
+});
+
+
+
+
+client.on('message', async message => {
+  if(message.content.startsWith(prefix + "تقديم")) {
+    await message.channel.send("**ما اسمك**").then(e => {
+    let filter = m => m.author.id === message.author.id
+    let lan = '';
+    let md = '';
+    let br = '';
+    let chaLan = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })
+    .then(collected => {
+      lan = collected.first().content
+      collected.first().delete()
+e.edit(`**ما خبرتك**`)
+let chaMd = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })
+.then(co => {
+  md = co.first().content
+        co.first().delete()
+        e.edit(`**موافق على اعطاء 10 بالميه من ارباحك للادارة**`)
+let br = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })
+.then(col => {
+  br = col.first().content
+        col.first().delete()
+e.edit("**جاري التقديم علي طلبك...**").then(b => {
+        setTimeout(() => {
+  b.edit(`**تم التقديم وسيتم الرد فـ اقرب وقت**`)
+        },2000);
+var gg = message.guild.channels.find('name', 'التقديمات')
+if(!gg) return;
+if(gg) {
+gg.send({embed : new Discord.RichEmbed()
+.setDescription(`**  الاسم : \n ${lan}\nاالخبره :\n ${md} \nموافقة 10 بالميه من الارباح :\n ${br}  \nتم التقديم بواسطة  : <@${message.author.id}> **`)  
+          .setFooter(`!DOLLAR  STORE`)
+.setTimestamp()
+});
+}        
+})
+})
+})
+})
+})
+ }
+})
     
 
 
